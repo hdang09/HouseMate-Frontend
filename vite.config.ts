04bcd0fa2946908475d2d18ac13dcac6dec8efd7
hdang09 <dangtranhai628@gmail.com>
2023-09-16 00:43:10 +0700

    UPDATE - Add swc config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
 });
